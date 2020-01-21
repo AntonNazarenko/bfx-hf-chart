f442f08bff08550fc0b743aa232720551e6e8afb
Cris Mihalache <me@f3rno.com>
2020-01-21 21:22:33 +0700

    Updates

---
+++ precache-manifest.3c213a2b1456152fb45c76e504be23ec.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "740cb95ea23ec001f6455af5f360f2fd",
+    "url": "/react-gh-pages/index.html"
+  },
+  {
+    "revision": "fecb90da0b14f5e90b59",
+    "url": "/react-gh-pages/static/css/main.269d7ac2.chunk.css"
+  },
+  {
+    "revision": "1e467fe1ab958df9f186",
+    "url": "/react-gh-pages/static/js/2.19aeeefe.chunk.js"
+  },
+  {
+    "revision": "30ef00582bb43f66473d6f827af8e64c",
+    "url": "/react-gh-pages/static/js/2.19aeeefe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fecb90da0b14f5e90b59",
+    "url": "/react-gh-pages/static/js/main.9ae09be1.chunk.js"
+  },
+  {
+    "revision": "c9d53a663ec051dd4010",
+    "url": "/react-gh-pages/static/js/runtime-main.5abed82c.js"
+  },
+  {
+    "revision": "97bc5dafef1665d72ea2157a76bdd43d",
+    "url": "/react-gh-pages/static/media/untitled-font-6.97bc5daf.svg"
+  },
+  {
+    "revision": "9fc20fd7a16893ab4d0683f5804540f9",
+    "url": "/react-gh-pages/static/media/untitled-font-6.9fc20fd7.eot"
+  },
+  {
+    "revision": "bbb2e18d6a35e6ace1081b7dc9817570",
+    "url": "/react-gh-pages/static/media/untitled-font-6.bbb2e18d.woff"
+  },
+  {
+    "revision": "cd0b925cee1e89e0187226ed5b59162e",
+    "url": "/react-gh-pages/static/media/untitled-font-6.cd0b925c.ttf"
+  }
+]);
